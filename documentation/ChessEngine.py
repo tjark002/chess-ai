1c254f3b2257840009b9b08697baa1d7af6a1ca1
tjark002 <49152482+tjark002@users.noreply.github.com>
2024-05-11 21:56:11 +0200

    PVS instead of minimax, time measuring

--- documentation/ChessEngine.py
+++ documentation/ChessEngine.py
@@ -11,13 +11,16 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
 
+from joblib import Parallel, delayed
+from collections import defaultdict
+
 class Engine:
 
-    def __init__(self, board, maxDepth, color):
+    def __init__(self, board, maxDepth, model=None):
         self.board = board
-        self.color = color
         self.maxDepth = maxDepth
         self.evalFunct = self.evalFunct
+        self.model = model
         self.neuralNet = cn.ChessNet(128)
         # Mapping from piece type to index in the board3d array
         self.piece_to_index = {
@@ -35,17 +38,17 @@ class Engine:
         print("Output of the network:", output)
         return output
     
-    def evalFunct(self):
+    def evalFunct(self, color):
         compt = 0
         #Sums up the material values
         for i in range(64):
-            compt+=self.squareResPoints(ch.SQUARES[i])
-        compt += self.mateOpportunity() + self.openning()
+            compt+=self.squareResPoints(ch.SQUARES[i], color)
+        compt += self.mateOpportunity(color) + self.openning(color)
         return compt
 
-    def mateOpportunity(self):
+    def mateOpportunity(self, color):
         if (self.board.legal_moves.count()==0):
-            if (self.board.turn == self.color):
+            if (self.board.turn == color):
                 return -999
             else:
                 return 999
@@ -53,9 +56,9 @@ class Engine:
             return 0
 
     #to make the engine developp in the first moves
-    def openning(self):
+    def openning(self, color):
         if (self.board.fullmove_number<10):
-            if (self.board.turn == self.color):
+            if (self.board.turn == color):
                 return 1/30 * self.board.legal_moves.count()
             else:
                 return -1/30 * self.board.legal_moves.count()
@@ -65,7 +68,7 @@ class Engine:
     #Takes a square as input and 
     #returns the corresponding Hans Berliner's
     #system value of it's resident
-    def squareResPoints(self, square):
+    def squareResPoints(self, square, color):
         pieceValue = 0
         if(self.board.piece_type_at(square) == ch.PAWN):
             pieceValue = 1
@@ -78,17 +81,262 @@ class Engine:
         elif (self.board.piece_type_at(square) == ch.QUEEN):
             pieceValue = 8.8
 
-        if (self.board.color_at(square)!=self.color):
+        if (self.board.color_at(square)!=color):
             return -pieceValue
         else:
             return pieceValue
 
         
-    def getBestMove(self):
+    def getBestMove(self, color, ai_mode=True):
+        board = self.board
         # Always initialize move to a valid chess.Move object or None
-        best_move, movecount = self.get_ai_move(self.board, self.maxDepth)
-        return best_move
+        max_move = None
+        max_eval = -np.inf
 
+        i = 0
+        for move in board.legal_moves:
+            board.push(move)
+            eval, i = self.minimax(board, self.maxDepth - 1, -np.inf, np.inf, False, i, color, ai_mode)
+            board.pop()
+            if eval > max_eval:
+                max_eval = eval
+                max_move = move
+
+        return max_move, i
+    
+    # Prediction function
+    def predict(self, board3d):
+        # Ensure the input has the correct shape and add a batch dimension
+        board_tensor = torch.tensor(board3d, dtype=torch.float32)
+
+        # Make prediction
+        with torch.no_grad():
+            prediction = self.model(board_tensor)
+
+        return prediction.item()
+
+    #Pytorch optimized minimax
+    def minimax_eval_ai(self, board, transposition_table=defaultdict(lambda: None)):
+        fen = board.fen()
+        cached_eval = transposition_table.get(fen)
+        if cached_eval is not None:
+            return cached_eval
+        board3d = self.split_dims(board)
+        board3d = np.expand_dims(board3d, 0)
+        pred = self.predict(board3d)
+
+        # Store the evaluation in the transposition table
+        transposition_table[fen] = pred
+        return pred
+    
+    #Pytorch optimized minimax
+    def minimax_eval(self, board, color):
+        return self.evalFunct(color)
+
+    def minimax(self, board, depth, alpha, beta, maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None)):
+        if depth == 0 or board.is_game_over():
+            if ai_mode:
+                eval = self.minimax_eval_ai(board, transposition_table)
+            else:
+                eval = self.minimax_eval(board, color)
+            return eval, i+1
+
+        if maximizing_player:
+            max_eval = -np.inf
+            #for move in board.legal_moves:
+            for move in self.move_ordering(board, maximizing_player):
+                board.push(move)
+                eval, i = self.minimax(board, depth - 1, alpha, beta, False, i, color, ai_mode)
+                board.pop()
+                max_eval = max(max_eval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
+            return max_eval, i+1
+        else:
+            min_eval = np.inf
+            #for move in board.legal_moves:
+            for move in self.move_ordering(board, maximizing_player):
+                board.push(move)
+                eval, i = self.minimax(board, depth - 1, alpha, beta, True, i, color, ai_mode)
+                board.pop()
+                min_eval = min(min_eval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
+            return min_eval, i+1
+        
+    # Principal Variation Search (PVS) with Alpha-Beta Pruning and Move Ordering
+    def pvs(self, board, depth, alpha, beta, maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None)):
+        if depth == 0 or board.is_game_over():
+            if ai_mode:
+                eval = self.minimax_eval_ai(board, transposition_table)
+            else:
+                eval = self.minimax_eval(board, color)
+            return eval, i+1
+
+        legal_moves = self.move_ordering(board, maximizing_player)
+        if not legal_moves:
+            return (0 if board.is_check() else 0.5), i
+
+        first_move = True
+        best_value = -np.inf if maximizing_player else np.inf
+
+        for move in legal_moves:
+            board.push(move)
+            if first_move:
+                value, i = self.pvs(board, depth - 1, alpha, beta, not maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None))
+                first_move = False
+            else:
+                if maximizing_player:
+                    value, i = self.pvs(board, depth - 1, alpha, alpha + 1, not maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None))
+                    if alpha < value < beta:
+                        value, i = self.pvs(board, depth - 1, value, beta, not maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None))
+                else:
+                    value, i = self.pvs(board, depth - 1, beta - 1, beta, not maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None))
+                    if alpha < value < beta:
+                        value, i = self.pvs(board, depth - 1, alpha, value, not maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None))
+            board.pop()
+
+            if maximizing_player:
+                best_value = max(best_value, value)
+                alpha = max(alpha, value)
+            else:
+                best_value = min(best_value, value)
+                beta = min(beta, value)
+
+            if beta <= alpha:
+                break
+
+        return best_value, i + 1
+
+
+    def get_ai_move(self, board, depth, color):
+        max_move = None
+        max_eval = -np.inf
+        # Optimized evaluation function with transposition table
+        transposition_table = defaultdict(lambda: None)
+
+        i = 0
+        for move in board.legal_moves:
+            board.push(move)
+            eval, i = self.pvs(board, depth - 1, -np.inf, np.inf, False, i, color, True, transposition_table)
+            board.pop()
+            if eval > max_eval:
+                max_eval = eval
+                max_move = move
+
+        return max_move, i
+    
+
+    # Move ordering heuristic function
+    def move_ordering(self, board, maximizing_player):
+        # Assign a score to each move
+        def move_score(move):
+            # Prioritize captures and checks
+            score = 0
+            if board.is_capture(move):
+                score += 10
+            if board.gives_check(move):
+                score += 5
+            return score
+
+        moves = list(board.legal_moves)
+        moves.sort(key=move_score, reverse=True)
+        return moves
+
+
+
+
+
+
+
+
+    
+    def square_to_indices(self, square):
+        return 7 - chess.square_rank(square), chess.square_file(square)
+
+    # Optimized `split_dims` function
+    def split_dims(self, board):
+        board3d = np.zeros((14, 8, 8), dtype=np.int8)
+
+        # Populate board3d for white pieces
+        for piece_type, index in self.piece_to_index.items():
+            squares = np.array(list(board.pieces(piece_type, chess.WHITE)))
+            if squares.size:
+                rows, cols = 7 - squares // 8, squares % 8
+                board3d[index, rows, cols] = 1
+
+        # Populate board3d for black pieces
+        for piece_type, index in self.piece_to_index.items():
+            squares = np.array(list(board.pieces(piece_type, chess.BLACK)))
+            if squares.size:
+                rows, cols = 7 - squares // 8, squares % 8
+                board3d[index + 6, rows, cols] = 1
+
+        # Add attacks/moves layers
+        aux = board.turn
+        board.turn = chess.WHITE
+        for move in board.legal_moves:
+            row, col = self.square_to_indices(move.to_square)
+            board3d[12, row, col] = 1
+        board.turn = chess.BLACK
+        for move in board.legal_moves:
+            row, col = self.square_to_indices(move.to_square)
+            board3d[13, row, col] = 1
+        board.turn = aux
+
+        return board3d
+
+    def fen_to_board(self, fen):
+        fen_board = chess.Board(fen)
+        return fen_board
+
+    def fen_to_board3d_array(self, fens):
+        board3d_array = np.zeros(shape=(fens.shape[0], 14, 8, 8))
+        for i in range(fens.shape[0]):
+            #print(fen)
+            fen_board = self.fen_to_board(fens[i])
+            board_split = self.split_dims(fen_board)
+            #print(board_split.shape)
+            board3d_array[i] = board_split
+
+        return board3d_array
+
+
+    def get_fen_dataset(self, path):
+        df = pd.read_csv(path)
+        df.head()
+        return df
+    
+
+
+
+
+
+    # Minimax function with Parallelization
+    def pvs_parallel(self, board, depth, alpha, beta, maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None)):
+        results = Parallel(n_jobs=8, prefer="threads")(delayed(self.pvs_single)(board, move, depth, alpha, beta, maximizing_player, i, color, ai_mode, transposition_table) for move in board.legal_moves)
+        best_result = max(results, key=lambda x: x[0])
+        max_eval, max_move, total_nodes = best_result
+        total_nodes = sum(x[2] for x in results)
+        return max_move, total_nodes
+
+    # Helper function for parallel processing
+    def pvs_single(self, board, move, depth, alpha, beta, maximizing_player, i, color, ai_mode=True, transposition_table=defaultdict(lambda: None)):
+        board.push(move)
+        eval, nodes = self.pvs(board, depth - 1, alpha, beta, not maximizing_player, i, color, ai_mode, transposition_table)
+        board.pop()
+        return eval, move, nodes
+    
+    # Function to get the best move using Principal Variation Search
+    def get_ai_move(self, board, depth, color, transposition_table=defaultdict(lambda: None)):
+        max_move, total_nodes = self.pvs_parallel(board, depth, -np.inf, np.inf, True, 0, color, True, transposition_table)
+        return max_move, total_nodes
+
+############################################################################################################
+# Legacy code
+############################################################################################################
     def engine(self, candidate, depth):
 
         # Get list of legal moves of the current position
@@ -131,130 +379,3 @@ class Engine:
                 break
 
         return best_move if depth == 1 else best_candidate
-    
-    # Prediction function
-    def predict(self, model, board3d):
-        # Ensure the input has the correct shape and add a batch dimension
-        board_tensor = torch.tensor(board3d, dtype=torch.float32)
-
-        # Make prediction
-        with torch.no_grad():
-            prediction = model(board_tensor)
-
-        return prediction.item()
-
-    #Pytorch optimized minimax
-    def minimax_eval_ai(self, board):
-        #start = timer.time()
-        board3d = self.split_dims(board)
-        #splittime = timer.time() - start
-        board3d = np.expand_dims(board3d, 0)
-        #expand_dims_time = timer.time() - splittime - start
-        pred = self.predict(board3d)
-        #pred_time = timer.time() - splittime - expand_dims_time - start
-
-        #print(f"Split time in seconds: {splittime:.8f},  expand dims time in seconds: {expand_dims_time:.8f}, pred time in seconds: {pred_time:.8f}")
-        return pred
-    
-    #Pytorch optimized minimax
-    def minimax_eval(self, board):
-        return self.evalFunct()
-
-    def minimax(self, board, depth, alpha, beta, maximizing_player, i):
-        if depth == 0 or board.is_game_over():
-            eval = self.minimax_eval(board)
-            return eval, i+1
-
-        if maximizing_player:
-            max_eval = -np.inf
-            for move in board.legal_moves:
-                board.push(move)
-                eval, i = self.minimax(board, depth - 1, alpha, beta, False, i)
-                board.pop()
-                max_eval = max(max_eval, eval)
-                alpha = max(alpha, eval)
-                if beta <= alpha:
-                    break
-            return max_eval, i+1
-        else:
-            min_eval = np.inf
-            for move in board.legal_moves:
-                board.push(move)
-                eval, i = self.minimax(board, depth - 1, alpha, beta, True, i)
-                board.pop()
-                min_eval = min(min_eval, eval)
-                beta = min(beta, eval)
-                if beta <= alpha:
-                    break
-            return min_eval, i+1
-
-    def get_ai_move(self, board, depth):
-        max_move = None
-        max_eval = -np.inf
-
-        i = 0
-        for move in board.legal_moves:
-            board.push(move)
-            eval, i = self.minimax(board, depth - 1, -np.inf, np.inf, False, i)
-            board.pop()
-            if eval > max_eval:
-                max_eval = eval
-                max_move = move
-
-        return max_move, i
-    
-    def square_to_indices(self, square):
-        return 7 - chess.square_rank(square), chess.square_file(square)
-
-    # Optimized `split_dims` function
-    def split_dims(self, board):
-        board3d = np.zeros((14, 8, 8), dtype=np.int8)
-
-        # Populate board3d for white pieces
-        for piece_type, index in self.piece_to_index.items():
-            squares = np.array(list(board.pieces(piece_type, chess.WHITE)))
-            if squares.size:
-                rows, cols = 7 - squares // 8, squares % 8
-                board3d[index, rows, cols] = 1
-
-        # Populate board3d for black pieces
-        for piece_type, index in self.piece_to_index.items():
-            squares = np.array(list(board.pieces(piece_type, chess.BLACK)))
-            if squares.size:
-                rows, cols = 7 - squares // 8, squares % 8
-                board3d[index + 6, rows, cols] = 1
-
-        # Add attacks/moves layers
-        aux = board.turn
-        board.turn = chess.WHITE
-        for move in board.legal_moves:
-            row, col = self.square_to_indices(move.to_square)
-            board3d[12, row, col] = 1
-        board.turn = chess.BLACK
-        for move in board.legal_moves:
-            row, col = self.quare_to_indices(move.to_square)
-            board3d[13, row, col] = 1
-        board.turn = aux
-
-        return board3d
-
-    def fen_to_board(self, fen):
-        fen_board = chess.Board(fen)
-        return fen_board
-
-    def fen_to_board3d_array(self, fens):
-        board3d_array = np.zeros(shape=(fens.shape[0], 14, 8, 8))
-        for i in range(fens.shape[0]):
-            #print(fen)
-            fen_board = self.fen_to_board(fens[i])
-            board_split = self.split_dims(fen_board)
-            #print(board_split.shape)
-            board3d_array[i] = board_split
-
-        return board3d_array
-
-
-    def get_fen_dataset(self, path):
-        df = pd.read_csv(path)
-        df.head()
-        return df
--- documentation/Main.py
+++ documentation/Main.py
@@ -1,10 +1,14 @@
 import ChessEngine as ce
 import chess as ch
+import torch
+from ChessNet import ChessNet
+import time as timer
 
 class Main:
 
-    def __init__(self, board=ch.Board):
+    def __init__(self, board=ch.Board, model=None):
         self.board=board
+        self.model=model
 
     #play human move
     def playHumanMove(self):
@@ -23,9 +27,18 @@ class Main:
             self.playHumanMove()
 
     #play engine move
-    def playEngineMove(self, maxDepth, color):
-        engine = ce.Engine(self.board, maxDepth, color)
-        self.board.push(engine.getBestMove())
+    def playEngineMove(self, maxDepth, color, engine=None, ai_mode=True):
+        start = timer.time()
+        best_move, move_count = engine.getBestMove(color, ai_mode)
+        end = timer.time()
+        elapsed_time = end - start
+        if ai_mode:
+            print(f"NEURAL NET: Movecount [{move_count}], Absolute time in seconds: {elapsed_time:.4f}, Moves per second: {move_count/elapsed_time:.2f}")
+        else:
+            print(f"BRUTEFORCE: Movecount [{move_count}], Absolute time in seconds: {elapsed_time:.4f}, Moves per second: {move_count/elapsed_time:.2f}")
+        self.board.push(best_move)
+
+        return move_count/elapsed_time
 
     #start a game
     def startGame(self):
@@ -68,16 +81,23 @@ class Main:
         maxDepth=None
         while(isinstance(maxDepth, int)==False):
             maxDepth = int(input("""Choose depth: """))
+        engine = ce.Engine(self.board, maxDepth, self.model)
+        i, j = 0, 0
+        ai_kps, bf_kps = 0, 0
         while (self.terminate_game()==False):
-            #print("The engine is thinking...")
-            self.playEngineMove(maxDepth, ch.WHITE)
+            #print("AI: The engine is thinking...")
+            ai_kps += self.playEngineMove(maxDepth, ch.WHITE, engine, True)
+            i += 1
             #print(self.board)
             if self.terminate_game()==True:
                 break
-            #print("The engine is thinking...")
-            self.playEngineMove(maxDepth, ch.BLACK)
+            #print("BRUTEFORCE: The engine is thinking...")
+            bf_kps += self.playEngineMove(maxDepth, ch.BLACK, engine, False)
+            j += 1
             #print(self.board)
         print("The game is over!")
+        print(f"AI: {i} moves, {ai_kps/i:.2f} KPS")
+        print(f"BRUTEFORCE: {j} moves, {bf_kps/j:.2f} KPS")
         print(self.board)
         print(self.board.outcome())
 
@@ -89,10 +109,14 @@ class Main:
         maxDepth=None
         while(isinstance(maxDepth, int)==False):
             maxDepth = int(input("""Choose depth: """))
+        engine = ce.Engine(self.board, maxDepth, self.model)
+        ai_mode=None
+        while(isinstance(ai_mode, bool)==False):
+            ai_mode = bool(input("""Choose ai_mode: """))
         if color=="b":
             while (self.terminate_game()==False):
                 print("The engine is thinking...")
-                self.playEngineMove(maxDepth, ch.WHITE)
+                self.playEngineMove(maxDepth, ch.WHITE, engine)
                 print(self.board)
                 if self.terminate_game()==True:
                     break
@@ -108,7 +132,7 @@ class Main:
                 if self.terminate_game()==True:
                     break
                 print("The engine is thinking...")
-                self.playEngineMove(maxDepth, ch.BLACK)
+                self.playEngineMove(maxDepth, ch.BLACK, engine)
             print(self.board)
             print(self.board.outcome())
 
@@ -129,7 +153,17 @@ class Main:
         else:
             return False
 
+
+# Load the model
+model = ChessNet(hidden_size=128)
+model.load_state_dict(torch.load('model.pth'))
+print("Model loaded successfully!")
+print(model)
+
 #create an instance and start a game
 newBoard= ch.Board()
-game = Main(newBoard)
+game = Main(newBoard, model)
+
+
+
 bruh = game.startGame()
